be5117a2122830a9c119cc946eb8ff9ebdd1dad7
Marcus Phillips <mracus@twitter.com>
2012-04-03 10:15:57 -0700

    deleted stray files

--- demotyper.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  var $input = $('.input');
-  var $output = $('.output');
-  $input.keypress(function(event){
-    $output.append($('<span>'+'a'+'</span>'));
-    console.log(event);
-  }).focus();
-});
